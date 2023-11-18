4dba49af3ff05bc6cd4db71a8f0629493018b1da
opu3199 <mdhassan.rakib2018@gmail.com>
2023-11-18 22:27:24 +0600

    added token

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const cors=require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
+
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${ process.env.USER_DATA}:${ process.env.USER_PASS}@cluster0.nhy8bre.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,18 +33,87 @@ async function run() {
     const reviwecollection = client.db("Bistroboss").collection("reviwe")
     const cartcollection = client.db("Bistroboss").collection("cart")
     const usercollection = client.db("Bistroboss").collection("user")
-    
+
+
+    //JWT API
+    // app.post('/jwt',async (req,res)=>{
+    //   const user=req.body
+    //   const token=jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+    //   res.send({token})
+    // })
+
+    // //middleware
+    // const verifytoken=(req,res,next)=>{
+    //   console.log('inside verify token',req.headers)
+    //   if(!req.headers.Authorization){
+    //     return res.status(401).send({message:'forbidden access'})
+    //   }
+    //   const token=req.headers.Authorization.split(' ')[1]
+    //   jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+    //     if(err){
+    //       return res.status(401).send({message:'forbidden access'})
+    //     }
+    //     req.decoded=decoded
+    //     next()
+    //   })
+    // }
+
+
     
 
     app.post('/user',async(req,res)=>{
       const user=req.body
+      //insert email
+
+      const query = {email:user.email}
+      const existinguser=await usercollection.findOne(query)
+      if(existinguser){
+        return res.send({message:'user already exist',insertedId:null})
+      }
       const result=await usercollection.insertOne(user)
       res.send(result)
     })
+
+    //user realted
+
+    app.patch('/user/admin/:id',async(req,res)=>{
+      const id=req.params.id
+      const filter = {_id:new ObjectId(id)}
+      const upadtedoc={
+        $set:{
+          role:'admin',
+        }
+      }
+      const result =await usercollection.updateOne(filter,upadtedoc)
+      res.send(result)
+    })
+
+    //get user data
+
+    app.get('/user',async(req,res)=>{
+      const result=await usercollection.find().toArray()
+      res.send(result)
+
+    })
+
+    //delete user
+    app.delete('/user/:id',async(req,res)=>{
+      const id =req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await usercollection.deleteOne(query)
+      res.send(result)
+
+    })
+
+
+
+    //menu get
+
     app.get('/menu',async(req,res)=>{
         const result=await menucollection.find().toArray()
         res.send(result)
     })
+
     app.get('/reviwe',async(req,res)=>{
         const result=await reviwecollection.find().toArray()
         res.send(result)
